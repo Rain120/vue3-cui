687db71db867a2203870a76a9c0a48a9c8a3b31e
Rain120 <chao1202016@outlook.com>
2019-07-15 23:59:15 +0800

    add: vue-router

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    port: 9527
+  },
+
   pages: {
     index: {
       entry: 'examples/main.ts',
@@ -15,8 +19,7 @@ module.exports = {
       .set('~', path.resolve('packages'));
 
     config.module
-      .rule('ts')
-      .test('/\.ts$/')
+      .rule('/\.ts|.tsx$/')
       .include.add(/packages/).end()
       .include.add(/examples/).end()
       .use('ts-loader')
